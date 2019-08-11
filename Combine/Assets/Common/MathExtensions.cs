c5546d632b7b4a0f5d30065a7c096c54825f2c76
sam6321 <sam6321@live.com.au>
2019-08-11 11:07:10 +0800

    Added initial

---
+++ Combine/Assets/Common/MathExtensions.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    /// <summary>
+    /// Contains general math functions to extend the default <see cref="Mathf"/> ones
+    /// </summary>
+    static class MathExtensions
+    {
+        /// <summary>
+        /// Clamp the magnitude of a value, regardless of its sign.
+        /// </summary>
+        /// <param name="value">The value to clamp</param>
+        /// <param name="min">The minimum magnitude of the value. This should be positive</param>
+        /// <param name="max">The maximum magnitude of the value. This should be positive</param>
+        /// <returns>Magnitude clamped value</returns>
+        public static float ClampMagnitude(float value, float min, float max)
+        {
+            return Mathf.Clamp(Mathf.Abs(value), min, max) * Mathf.Sign(value);
+        }
+
+        /// <summary>
+        /// Interpolate between colours using HSV rather than RGB.
+        /// This interpolates in a nicer way than a raw RGB lerp
+        /// </summary>
+        /// <param name="from">Colour to interpolate from</param>
+        /// <param name="to">Colour to interpolate to</param>
+        /// <param name="f">The interpolation factor between 0 and 1</param>
+        /// <returns>The HSV interpolated colour</returns>
+        public static Color LerpHSV(Color from, Color to, float f)
+        {
+            Color.RGBToHSV(from, out float fromH, out float fromS, out float fromV);
+            Color.RGBToHSV(to, out float toH, out float toS, out float toV);
+
+            return Color.HSVToRGB
+            (
+                Mathf.Lerp(fromH, toH, f),
+                Mathf.Lerp(fromS, toS, f),
+                Mathf.Lerp(fromV, toV, f)
+            );
+        }
+
+        /// <summary>
+        /// Gets the interpolation factor that <see cref="value"/> is between <see cref="from"/> and <see cref="too"/>
+        /// and passes it through a smoothstep function.
+        /// </summary>
+        /// <param name="from">Lower bound</param>
+        /// <param name="to">Upper bound</param>
+        /// <param name="value">The value between from and to</param>
+        /// <returns>Smoothstepped interpolation factor</returns>
+        public static float InverseLerpSmoothstep(float from, float to, float value)
+        {
+            return Mathf.SmoothStep(0, 1, Mathf.InverseLerp(from, to, value));
+        }
+    }
+}
---
+++ Combine/Assets/Common/RangeFloat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common
+{
+    /// <summary>
+    /// Stores a pair of values representing the start and end of a range.
+    /// The pair can be set via the Unity inspector.
+    /// </summary>
+    [Serializable]
+    public class RangeFloat
+    {
+        /// <summary>
+        /// Start range value. Should be less than the end range value.
+        /// </summary>
+        public float start;
+
+        /// <summary>
+        /// End range value. Should be greater than the start range value.
+        /// </summary>
+        public float end;
+
+        /// <summary>
+        /// Construct a range pair
+        /// </summary>
+        /// <param name="start">Start range value</param>
+        /// <param name="end">End range value</param>
+        public RangeFloat(float start, float end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        /// <summary>
+        /// Clamp a value to the range
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <returns>Value clamped to the range</returns>
+        public float Clamp(float value)
+        {
+            return Mathf.Clamp(value, start, end);
+        }
+
+        /// <summary>
+        /// Get the inverse lerp factor that is between start and end.
+        /// This is the inverse of <see cref="Lerp(float)"/>
+        /// </summary>
+        /// <param name="value">The value betwen start and end</param>
+        /// <returns>The factor, as a value between 0 and 1</returns>
+        public float InverseLerp(float value)
+        {
+            return Mathf.InverseLerp(start, end, value);
+        }
+
+        /// <summary>
+        /// Interpolate between start and end by the given factor.
+        /// This can be inverted with <see cref="InverseLerp(float)"/>
+        /// </summary>
+        /// <param name="factor">The interpolation factor, between 0 and 1</param>
+        /// <returns>The value between start and end corresponding to the interpolation factor</returns>
+        public float Lerp(float factor)
+        {
+            return Mathf.Lerp(start, end, factor);
+        }
+
+        /// <summary>
+        /// Get a random value between the range values
+        /// </summary>
+        /// <returns>The random value</returns>
+        public float Random()
+        {
+            return UnityEngine.Random.Range(start, end);
+        }
+    }
+}
+
